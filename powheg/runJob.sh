e81b088b1ddab8298b04fab2153164323103a546
Shin-Shan Yu <syu@ncu200.cern.ch>
2014-10-30 20:01:40 +0100

    modify so that local input files could be used

--- powheg/runJob.sh
+++ powheg/runJob.sh
@@ -5,6 +5,13 @@ cd $1
 export SCRAM_ARCH=slc6_amd64_gcc481; eval `scramv1 runtime -sh`
 
 read -d "\n" a b c d e f g < $2
+
+if [[ $d == *PWD* ]]; then
+ sub=`echo "${d##*/}"`
+ d=$1"/"$sub
+ echo $d
+fi
+
 ./create_powheg_tarball.sh $a $b $c $d $e $f $g
 
 
