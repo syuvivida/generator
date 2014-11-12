b60b77c4f7dfcad4d5e6097344aba10e4da412a4
Shin-Shan Yu <syu@ncu200.cern.ch>
2014-11-12 16:40:50 +0100

    change the source tar ball name

--- powheg/slc6_DMGG.py
+++ powheg/slc6_DMGG.py
@@ -28,7 +28,7 @@ process.externalLHEProducer.numberOfParameters = cms.uint32(5)
 process.externalLHEProducer.outputFile = cms.string('events_final.lhe')
 
 process.externalLHEProducer.args = cms.vstring('slc6_amd64_gcc481/powheg/V2.0/src',
-                                               'powhegboxV2_Sep2014',
+                                               'powhegboxV2_Nov2014',
                                                'DMGG',
                                                'slc6_amd64_gcc481/powheg/V2.0/13TeV/examples/DMGG_NNPDF30_13TeV/DMGG_NNPDF30_13TeV.input',
                                                'eiko_DMGG'
